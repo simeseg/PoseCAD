ce9b9c2e0f77ade4e9cd6516bbfa846991c063de
simeseg <48680907+simeseg@users.noreply.github.com>
2019-05-30 17:43:53 +0900

    Update detectKeypoints.h

--- detectKeypoints.h
+++ detectKeypoints.h
@@ -2,7 +2,7 @@
 #ifndef DETECTKEYPOINTS_H_INCLUDED
 #define DETECTKEYPOINTS_H_INCLUDED
 
-//declaration of Eulidean clustering
+//declaration of Keypoint detection
 pcl::PointCloud<pcl::PointNormal>::Ptr detectKeypoints (const pcl::PointCloud<pcl::PointXYZ>& points, float min_scale, int nr_octaves, int nr_scales_per_octave, float min_contrast);
 
 #endif
